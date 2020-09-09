ff75fda1ba6f9a6e04383efe6f76e626e8f151c5
Rakeshreddy Velamala <48100161+rakeshreddy3334@users.noreply.github.com>
2020-09-09 15:17:08 +0530

    Add files via upload

---
+++ source_code/code.ino
@@ -0,0 +1,166 @@
+#include <TinyGPS++.h>
+#include <SoftwareSerial.h>
+#define BLYNK_PRINT Serial
+#include <ESP8266WiFi.h>
+#include <BlynkSimpleEsp8266.h>
+#include <ESP8266WiFi.h>
+#include <OneWire.h>
+#include <PubSubClient.h>
+
+static const int RXPin = 4, TXPin = 5;   // GPIO 4=D2(conneect Tx of GPS) and GPIO 5=D1(Connect Rx of GPS
+static const uint32_t GPSBaud = 9600; //if Baud rate 9600 didn't work in your case then use 4800
+const char *ssid =  "ACT101014564385";        //Your Access Point or Personal Hotspot, cannot be longer than 32 characters!
+const char *pass =  "44017881";    //Your Access Point or Personal Hotspot password
+const char* serverTS = "api.thingspeak.com";
+char auth[] = "aa26238b0db54c69ae84a952d46d981c";  
+String apiKey = "HWP2ST3URKAB3QQE";          //Insert your Channel API Key here
+int TRIGGER = 2; //Pin D4 = TRIGGER
+int ECHO = 14; //Pin D5 = ECHO
+static const float bin=25;
+
+TinyGPSPlus gps; // The TinyGPS++ object
+WidgetMap myMap(V0);  // V0 for virtual pin of Map Widget
+
+SoftwareSerial ss(RXPin, TXPin);  // The serial connection to the GPS device
+
+BlynkTimer timer;
+
+float spd;       //Variable  to store the speed
+float sats;      //Variable to store no. of satellites response
+String bearing;  //Variable to store orientation or direction of GPS
+
+
+unsigned int move_index = 1;       // fixed location for now
+
+
+void setup()
+{
+  Serial.begin(115200);
+  Serial.println();
+  ss.begin(GPSBaud);
+  Blynk.begin(auth, ssid, pass);
+  timer.setInterval(5000L, checkGPS); // every 5s check if GPS is connected, only really needs to be done once
+  pinMode(0,OUTPUT);            //LED connected to GPIO2
+  pinMode(TRIGGER,OUTPUT);
+  pinMode(ECHO,INPUT);
+} 
+
+void checkGPS(){
+  if (gps.charsProcessed() < 10)
+  {
+    Serial.println(F("No GPS detected: check wiring."));
+      Blynk.virtualWrite(V4, "GPS ERROR");  // Value Display widget  on V4 if GPS not detected
+  }
+}
+
+void loop()
+{
+ 
+    while (ss.available() > 0) 
+    {
+      // sketch displays information every time a new sentence is correctly encoded.
+      if (gps.encode(ss.read()))
+        displayInfo();
+  }
+  Blynk.run();
+  timer.run();
+  // establish variables for duration of the ping,
+  // and the distance result in inches and centimeters:
+  long duration, inches, cm,percentage;
+
+  // The PING))) is triggered by a HIGH pulse of 2 or more microseconds.
+  // Give a short LOW pulse beforehand to ensure a clean HIGH pulse:
+ 
+  digitalWrite(TRIGGER, LOW);
+  delayMicroseconds(2);
+  digitalWrite(TRIGGER, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(TRIGGER, LOW);
+
+  // The same pin is used to read the signal from the PING))): a HIGH
+  // pulse whose duration is the time (in microseconds) from the sending
+  // of the ping to the reception of its echo off of an object.
+
+  duration = pulseIn(ECHO, HIGH);
+
+  // convert the time into a distance
+  cm = microsecondsToCentimeters(duration);
+  percentage=(1-(cm/bin))*100;
+  Serial.print(cm);
+  Serial.print("cm");
+  Serial.println();
+
+  delay(100);
+
+  digitalWrite(2, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(1000);              // wait for a second
+  digitalWrite(2, LOW);    // turn the LED off by making the voltage LOW
+  delay(1000);              // wait for a second
+
+  sendHeight(percentage);
+}
+
+void displayInfo()
+{ 
+
+  if (gps.location.isValid() ) 
+  {
+    
+    float latitude = (gps.location.lat());     //Storing the Lat. and Lon. 
+    float longitude = (gps.location.lng()); 
+    
+    Serial.print("LAT:  ");
+    Serial.println(latitude, 6);  // float to x decimal places
+    Serial.print("LONG: ");
+    Serial.println(longitude, 6);
+    Blynk.virtualWrite(V1, String(latitude, 6));   
+    Blynk.virtualWrite(V2, String(longitude, 6));  
+    myMap.location(move_index, latitude, longitude, "GPS_Location");
+    spd = gps.speed.kmph();               //get speed
+       Blynk.virtualWrite(V3, spd);
+       
+       sats = gps.satellites.value();    //get number of satellites
+       Blynk.virtualWrite(V4, sats);
+
+       bearing = TinyGPSPlus::cardinal(gps.course.value()); // get the direction
+       Blynk.virtualWrite(V5, bearing);              
+  }
+  
+  Serial.println();
+}
+
+long microsecondsToCentimeters(long microseconds) {
+  // The speed of sound is 340 m/s or 29 microseconds per centimeter.
+  // The ping travels out and back, so to find the distance of the
+  // object we take half of the distance travelled.
+  return microseconds / 29 / 2;
+}
+
+ void sendHeight(float cm)
+{  
+  WiFiClient tclient;//not to be confused with "client" in PubSub{}, and wclient for mqtt
+   if (tclient.connect(serverTS, 80)) { // use ip 184.106.153.149 or api.thingspeak.com
+   Serial.println("WiFi Client connected ");
+   
+   String postStr = apiKey;
+   postStr += "&field1=";
+   postStr += String(cm);
+   postStr += "\r\n\r\n";
+   
+   tclient.print("POST /update HTTP/1.1\n");
+   tclient.print("Host: api.thingspeak.com\n");
+   tclient.print("Connection: close\n");
+   tclient.print("X-THINGSPEAKAPIKEY: " + apiKey + "\n");
+   tclient.print("Content-Type: application/x-www-form-urlencoded\n");
+   tclient.print("Content-Length: ");
+   tclient.print(postStr.length());
+   tclient.print("\n\n");
+   tclient.print(postStr);
+   delay(1000);  
+   }//end if
+
+ tclient.stop();
+}//end send to ts
+
+
+
